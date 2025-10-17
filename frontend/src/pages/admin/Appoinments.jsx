44fad40df20131dd8f9d355b9d4d1e72982f9d76
SenuriDis <vwahundeniya@gmail.com>
2025-10-17 17:01:18 +0530

    delete orignal files

--- frontend/src/pages/admin/Appoinments.jsx
+++
@@ -1,3 +0,0 @@
-export default function Appointments() {
-  return <h1 className="text-2xl font-bold text-blue-800">Appointments Overview</h1>;
-}
--- frontend/src/pages/admin/PatientRecods.jsx
+++
@@ -1,3 +0,0 @@
-export default function PatientRecords() {
-  return <h1 className="text-2xl font-bold text-blue-800">Patient Records Overview</h1>;
-}
