1e89653cf553c262fcc2d2f6ea853f200cdae37a
ViiHAA <vwahundeniya@gmail.com>
2025-10-17 08:43:56 +0530

    Fix environment variable names and update database connection paths in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const connectDB = require('./config/database');
-const medicalRoutes = require('./routes/medicalRecordRoutes');
+const connectDB = require('./src/config/database');
+const medicalRoutes = require('./src/routes/medicalRecordRoutes');
 
 const app = express();
 
